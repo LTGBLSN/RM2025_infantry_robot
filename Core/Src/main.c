67ce1c510dffaaf416a5eab8b165795bf6499af1
GruBaXu <108203631+LTGBLSN@users.noreply.github.com>
2025-03-19 01:07:28 +0800

    串口决定1个字节就触发一次中断，但是还没写

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -140,6 +140,7 @@ int main(void)
   MX_USART3_UART_Init();
   MX_CAN1_Init();
   MX_CAN2_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   remote_control_init();
   local_rc_ctrl = get_remote_control_point();
---
+++ code_mf/Src/uart_callback.c
@@ -0,0 +1,27 @@
+//
+// Created by 21481 on 2025/3/18.
+//
+#include "main.h"
+#include "cmsis_os.h"
+#include "can.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+
+#include <stdio.h>
+#include "board_LED.h"
+#include "uart_printf.h"
+#include "uart_sent.h"
+#include "bsp_rc.h"
+#include "remote_control.h"
+#include "get_rc.h"
+#include "bsp_can.h"
+#include "CAN_receive.h"
+
+
+void HAL_DMA_IdleCpltCallback(DMA_HandleTypeDef *hdma)
+{
+    /* 这里编写用户代码，处理DMA空闲中断 */
+}
+
+
