6a3f79e96a1c4781d0121b4750f823d388a7c157
GruBaXu <108203631+LTGBLSN@users.noreply.github.com>
2025-03-27 01:26:02 +0800

    更新拨杆控制弹仓盖

--- code_mf/Src/servo_task.c
+++ code_mf/Src/servo_task.c
@@ -50,13 +50,7 @@ void servo_task()
             }
         }
 
-
-
-
-
-
-
-        if((servo_state % 2) == 1 )
+        if(rc_s1 == 1)
         {
                     __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 500);
                     __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 500);
@@ -76,6 +70,27 @@ void servo_task()
                     __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, 1945);
                     __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_3, 1945);
         }
+
+//        if((servo_state % 2) == 1 )//°´¼üÅÐ¶Ï
+//        {
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 500);
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 500);
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_3, 500);
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_4, 500);
+//                    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_1, 500);
+//                    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, 500);
+//                    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_3, 500);
+//        }
+//        else
+//        {
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 1945);
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 1945);
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_3, 1945);
+//                    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_4, 1945);
+//                    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_1, 1945);
+//                    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, 1945);
+//                    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_3, 1945);
+//        }
 
         osDelay(1);
     }
